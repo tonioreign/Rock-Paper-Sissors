7b26052e55229a473b5687744b9155ad421371e5
tonioreign <tonioreign@gmail.com>
2022-04-15 12:14:02 -0700

    Added game() function

--- js/main.js
+++ js/main.js
@@ -1,13 +1,16 @@
 const choices = ["rock", "paper", "scissors"];
 
+//randomizes computer selection
 const computerPlay = () => {
   let random = Math.floor(Math.random() * choices.length);
-  console.log(choices[random]);
+  return choices[random];
 };
 
-computerPlay();
+//player vs computer
+//determines who wins the round
 
 const playRound = (playerSelection, computerSelection) => {
+  let playerChoice = playerSelection.toLowerCase();
   if (playerSelection === "scissors" && computerSelection === "paper") {
     console.log(`${playerSelection} beats ${computerSelection}! You Win!`);
   } else if (playerSelection === "rock" && computerSelection === "scissors") {
@@ -22,3 +25,34 @@ const playRound = (playerSelection, computerSelection) => {
     );
   }
 };
+
+//every round the score should be kept
+//should log the winner of every game
+
+const game = (playRound) => {
+  let score = 0;
+  let cscore = 0;
+  for (let round = 0; round < 5; round++) {
+    if (playerSelection > computerSelection) {
+      console.log("You won that round!");
+      score++;
+    } else if (computerSelection > playerSelection) {
+      console.log("You lost that round!");
+      cscore++;
+    } else {
+      continue;
+    }
+  }
+  if (score > cscore) {
+    console.log("You won the game!");
+  } else if (cscore > score) {
+    console.log("You lost the game!");
+  } else {
+    console.log("Its a draw!");
+  }
+};
+
+const playerSelection = "scissors";
+const computerSelection = computerPlay();
+
+game(playRound);
